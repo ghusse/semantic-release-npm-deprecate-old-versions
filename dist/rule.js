d83d63a93399a63cd92ca8cce060a8b6ed91703e
Guillaume Gautreau <guillaume@ghusse.com>
2020-10-17 21:31:48 +0200

    ci: :construction_worker: remove dist from commits, commit the changelog

--- dist/rule.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Action = void 0;
-var Action;
-(function (Action) {
-    Action["deprecate"] = "deprecate";
-    Action["keep"] = "keep";
-    Action["continue"] = "continue";
-})(Action = exports.Action || (exports.Action = {}));
