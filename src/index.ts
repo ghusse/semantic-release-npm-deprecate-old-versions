6638b7775e6db4aece0c5564667a0b737e04094a
Guillaume Gautreau <guillaume@ghusse.com>
2020-10-23 17:12:02 +0200

    fix: fix the instanciation of a service

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,20 @@ import { PackageInfoRetriever } from "./package-info-retriever";
 import { Authentifier } from "./authentifier";
 import VersionForMessageFinder from "./version-for-message-finder";
 import ConfigurationLoader from "./configuration-loader";
+import { deprecateAll } from "./rules/deprecate-all";
+import { supportLatest } from "./rules/support-latest";
+import { supportPreReleaseIfNotReleased } from "./rules/support-prerelease-if-not-released";
 
 const oldVersionDeprecier = new OldVersionDeprecier(
   new PackageInfoRetriever(),
   new RuleApplier(new VersionForMessageFinder()),
   new Deprecier(),
   new Authentifier(),
-  new ConfigurationLoader()
+  new ConfigurationLoader({
+    deprecateAll: deprecateAll,
+    supportLatest: supportLatest,
+    supportPreReleaseIfNotReleased: supportPreReleaseIfNotReleased,
+  })
 );
 
 module.exports = {
